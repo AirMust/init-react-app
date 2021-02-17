eab629fd667f350aeca3157ac78aa8e440e684cd
Айрат <airat.must@yandex.ru>
2021-02-17 13:32:24 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            src: path.resolve(__dirname, './src/'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    'postcss-preset-env',
+                                    'postcss-nested',
+                                    'postcss-simple-vars',
+                                    'postcss-color-mod-function',
+                                ],
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+        port: 3000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './www/index.html',
+        }),
+        new StylelintPlugin({
+            configFile: path.resolve(__dirname, './.stylelintrc.json'),
+            context: path.resolve(__dirname, './src'),
+        }),
+    ],
+};
